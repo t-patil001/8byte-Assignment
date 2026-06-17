9c96e315d0ec548885036541bc00af9223fde9a1
Tirthraj Patil <tirthraj@appscrip.co>
2026-06-18 00:57:22 +0530

    change version

--- app/backend/main.py
+++ app/backend/main.py
@@ -4,4 +4,8 @@ app = FastAPI()
 
 @app.get("/")
 def home():
+<<<<<<< HEAD
     return {"message": "Backend is running: v2"}
+=======
+    return {"message": "Backend is running: v1"}
+>>>>>>> 62b37d7 (change version)
